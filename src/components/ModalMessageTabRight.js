ce22fec220ec08d95ab8551a6b86897135073652
Proschansky <sam.proschansky@accesscfa.com>
2020-06-01 18:03:47 -0400

    Added the enter press key functionality.

--- src/components/ModalMessageTabRight.js
+++ src/components/ModalMessageTabRight.js
@@ -9,6 +9,12 @@ export default class ModalMessageTabRight extends React.Component{
         this.textRef = React.createRef();
     }
 
+    handleKeyPress = (state, event) =>{
+        if(event.key === 'Enter'){
+            state.handleNewMessage(state.activeMessageIndex, event) 
+        }
+    }
+
     priorMessages = (st, idx) => {
         
         if(idx !== undefined){
@@ -51,13 +57,13 @@ export default class ModalMessageTabRight extends React.Component{
                 (state.activeMessageIndex !== undefined) ?
                 <div className="tab-content" id="nav-tabContent">
                     <div id="sms_form">
-                        <form name="send_sms" action="" onSubmit={(e)=>{state.handleNewMessage(state.activeMessageIndex, e) }}>
+                        <form name="send_sms" id="smsForm"action="" onSubmit={(e)=>{state.handleNewMessage(state.activeMessageIndex, e) }}>
                         <input type="hidden" name="k" value="h7d22dh8d2h9d3j989j8hdh98d9h8d9h8d9hh98d2"/>
                         <input type="hidden" name="oid" value="1234"/>
                         <input type="hidden" name="toNumber" value="<?php echo $userid; ?>"/>
                         <input type="hidden" name="toNumber" value="+14049638521"/>
                         {this.priorMessages(state, state.activeMessageIndex)}
-                        <textarea className="form-control" style={{marginBottom: "1rem"}} value={state.currentMessage} onChange={state.handleInput}></textarea>
+                        <textarea className="form-control" style={{marginBottom: "1rem"}} value={state.currentMessage} onChange={state.handleInput} onKeyDown={(e)=>this.handleKeyPress(state, e)}></textarea>
                         <input type="submit" className="btn btn-primary float-right" name="sendSms" id="sendSms_btn" value="Send"/>		
                         </form>
                     </div>
