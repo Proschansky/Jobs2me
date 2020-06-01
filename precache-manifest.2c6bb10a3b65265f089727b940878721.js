da2a26051df60f102e5c65bfb89f1e765f9ba137
Proschansky <sam.proschansky@accesscfa.com>
2020-06-01 18:05:03 -0400

    Updates

---
+++ precache-manifest.2c6bb10a3b65265f089727b940878721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f1f5db10f4e993ff5f71b85210e510",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dc83ce647373616bb88",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9892f233d6bafa364283",
+    "url": "/static/js/2.008c34cb.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/static/js/2.008c34cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc83ce647373616bb88",
+    "url": "/static/js/main.81a3e72f.chunk.js"
+  },
+  {
+    "revision": "e91ba3d5e8cc7339bbc3",
+    "url": "/static/js/runtime-main.a094054c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Jobs2me/precache-manifest.facd8331e2c09918e824ee7b2763855f.js"
+  "/precache-manifest.2c6bb10a3b65265f089727b940878721.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jobs2me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
