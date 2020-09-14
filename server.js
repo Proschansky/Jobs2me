e47d2e6a1f408244c8f885b0b3e91acb52d87467
Proschansky <sam.proschansky@accesscfa.com>
2020-09-14 13:22:19 -0400

    Attempting to use elastic beanstalk.

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+//
+const path = require('path');
+const express = require('express');
+const PORT = process.env.HTTP_PORT || 4001;
+const app = express();
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html' ))
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}.`);
+});
